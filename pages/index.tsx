2107fd53b494b5fa54ca37d9bd226bccbc85663a
GJB65 <68871762+GJB65@users.noreply.github.com>
2021-06-17 13:49:21 +1000

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -42,8 +42,7 @@ export default function Home({
     <>
 
 <Hero
-		headlinebig = ""
-		headline ="Who are your internal and external stakeholders?"
+			headline ="Who are your internal and external stakeholders?"
 
 description ="To find out who your stakeholders are, develop a training plan for staff and key stakeholders on BCP and BIA. Ensure your organization manages ambiguity and consistently seeks to drive clarity amongst project stakeholders. Ensure your organization is responsible for supporting multiple stakeholders which include project, programs, operations, and functional organizations. Actively promote a risk aware culture with the values of stewardship, risk awareness and accountability through education and communication to all stakeholders. Organizations that lack effective cybersecurity measures to help manage this change, face breaking the trust of their (internal) customers, stakeholders, and the marketplace while exposing themselves to ever increasing risk. Collaborate and engage with various stakeholders to interpret maturity and the level of control implementation to promote adequately positioned controls and prioritize investments/effort through strong collaboration with Core IT Compliance and Enterprise Risk Management."
 />
